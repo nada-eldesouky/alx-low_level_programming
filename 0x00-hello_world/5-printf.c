79051d6d52c6a0b6221447852ab6b849594e066a
nada-eldesouky <nada-eldesouky@outlook.com>
2023-03-16 13:20:40 -0700

    blah5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry Point
  *
- * Return 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
 	
