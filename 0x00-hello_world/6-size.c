97716681fe56c52f845282bcb71ae90df2d96f84
nada-eldesouky <nada-eldesouky@outlook.com>
2023-03-16 16:56:07 -0700

    66

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-printf("size of char: %c byte(s)\n", sizeof(char));
-printf("size of int: %d byte(s)\n", sizeof(int));
-printf("size of long int: %li byte(s)\n", sizeof(long int));
-printf("size of long long int: %lli byte(s)\n", sizeof(long long int));	
-printf("size of float: %f byte(s)\n", sizeof(float));	
+printf("Size of char: %c byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %li byte(s)\n", sizeof(long int));
+printf("Size of long long int: %lli  byte(s)\n", sizeof(long long int));
+printf("Size of float: %f byte(s)\n", sizeof(float);
 return (0);
 }
