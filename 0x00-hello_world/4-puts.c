7724f20c32544ba78b80a3240a1b24b7633bc532
nada-eldesouky <nada-eldesouky@outlook.com>
2023-03-16 12:28:58 -0700

    2ndtry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -  put ("Programming is like building a multilingual puzzle"\n);
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-put ("Programming is like building a multilingual puzzle"\n);
+ put ("\"Programming is like building a multilingual puzzle"\n);
 Return (0);
 }
