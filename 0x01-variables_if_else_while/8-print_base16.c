0b0e386f9d26fb2737713e2db4b9c74cd75cc469
nada-eldesouky <nada-eldesouky@outlook.com>
2023-03-17 12:22:39 -0700

    jjkk

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,9 @@ int main(void)
 char i;
 int j;
 
-for (i = '0' ; i < '10' ; i++)
+for (i = 0 ; i < 10 ; i++)
 putchar(i + '0');	
-for (j != 'a ; j<= 'f' ; j++)
+for (j != 'a' ; j <= 'f' ; j++)
 putchar(j);
 putchar('\n');
 return (0);
