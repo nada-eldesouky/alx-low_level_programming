52359d03369ca9320eea29ac21c49c1cef1b48e1
nada-eldesouky <nada-eldesouky@outlook.com>
2023-03-17 12:51:40 -0700

    jiminaaaa

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,16 +5,21 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
-char i;
+int i;
 
-for (i = '0' ; i < '10' ; i++)
+for (i = 0 ; i < 10 ; i++)
 {
 putchar(i + '0');
+if (i != 9)
+{
 putchar(',');
 putchar(' ');
 }
+}
+putchar('\n');
 return (0);
 }
 
