9d15bf5f2648537cedca251c10977587585fb433
nada-eldesouky <nada-eldesouky@outlook.com>
2023-03-21 09:35:56 -0700

    00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * main - print _putchar
+ * main - main function
+ * _putchar
  * Return: 0
  */
 
@@ -13,5 +14,6 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+}
 _putchar('\n');
 return (0);
