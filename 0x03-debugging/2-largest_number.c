001120345a54a79007a665293f54eb8555fae228
nada-eldesouky <nada-eldesouky@outlook.com>
2023-03-23 02:48:28 -0700

    lma n4of

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,16 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (a > b && c > a)
-{
-largest = c;
-}
-else if (b > c)
+else if (a > c && c > b)
+largest = a;
+else if (b > a && a > c)
 {
 largest = b;
 }
+else if (b > c && c > a)
+largest = b;
+else if (c > a && a > b)
+largest = c;
 else
 {
 largest = c;
