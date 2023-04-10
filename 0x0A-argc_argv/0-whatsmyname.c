c34040551247faff582f6770f5a347701e25457c
nada-eldesouky <nada-eldesouky@outlook.com>
2023-04-10 13:37:37 -0700

    hmm

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-(void)argc;
-printf("%s\n", *argv);
+printf("%s\n", *argv[0]);
 return (0);
 }
