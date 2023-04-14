d85d209f7246e61f3f189ba7c39dd0471ab5cda6
nada-eldesouky <nada-eldesouky@outlook.com>
2023-04-14 09:07:09 -0700

    ok

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,29 +1,45 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
- * _strdup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the resulting string
  */
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *aaa;
-	int i, r = 0;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (str == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
-	i = 0;
-	while (str[i] != '\0')
+
+	while (i < len1)
+	{
+		s[i] = s1[i];
 		i++;
+	}
 
-	aaa = malloc(sizeof(char) * (i + 1));
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
 
-	if (aaa == NULL)
-		return (NULL);
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
 
-	for (r = 0; str[r]; r++)
-		aaa[r] = str[r];
+	s[i] = '\0';
 
-	return (aaa);
+	return (s);
 }
