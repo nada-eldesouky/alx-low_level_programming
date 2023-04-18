0b211ea9e2e51688cd394326bedf497b250ef4aa
nada-eldesouky <nada-eldesouky@outlook.com>
2023-04-18 13:01:45 -0700

    chimshim

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,13 +5,12 @@
 * @name: name to initialize
 * @age: age to initialize
 * @owner: owner to initialize
-*Return: void
 */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
-if (d)
-{
-d->name = name;
-d->age = age;
-d->owner = owner;
-}
+	void init_dog(struct dog *d, char *name, float age, char *owner)
+	{
+		if (d == NULL)
+			d = malloc(sizeof(struct dog));
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
