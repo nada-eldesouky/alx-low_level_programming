c52950f0d0204e3d33f7e2fc2ead7ea64a3a6511
nada-eldesouky <nada-eldesouky@outlook.com>
2023-04-17 18:01:04 -0700

    1111

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- *struct dog - a dog struct
- *@name: dog name
- *@age: dog age
- *@owner: owner name
- * Descreption: just a long day struct in a big kitty world
- */
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description:  A dog is the only thing on earth that loves you
+*/
 struct dog
-{
-char *name;
-float age;
-char *owner;
-);
+	{
+		char *name;
+		float age;
+		char *owner;
+	};
 #endif
