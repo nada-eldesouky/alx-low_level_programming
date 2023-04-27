aac041ca8cd771663b02d006247c96ca7a4fa447
nada-eldesouky <nada-eldesouky@outlook.com>
2023-04-27 16:02:09 -0700

    1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - lenght of ll
+ * @h: pointer to node
+ *
+ * Return: Size of list
+ */
+size_t list_len(const list_t *h)
+{
+size_t n = 0;
+while (h)
+{
+	h = h->next;
+	n++;
+}
+return (n);
+}
