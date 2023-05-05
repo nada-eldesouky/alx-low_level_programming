f0deb4f86cd4cd89eaaf2cc22e15601c132b7c46
nada-eldesouky <nada-eldesouky@outlook.com>
2023-05-05 08:26:40 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/*
+ * print_binary - print a number as a binary str
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n);
+
+{
+int mask = 1 << 31;
+int n;
+printf("Binary representation: ");
+
+for (n = 0; n < 32; n++)
+{
+if (num & mask)
+{
+printf("1");
+}
+else
+{
+printf("0");
+}
+mask >>= 1;
+}
+}
